ca65f7fab5f9518baa965812c15e6e7d405dddd1
Abduhamid <karalevstvabitva@gmail.com>
2025-07-08 00:13:06 +0500

    Cron Ubdate

--- src/auto-loud/auto-loud.controller.ts
+++ src/auto-loud/auto-loud.controller.ts
@@ -5,41 +5,34 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Controller('auto-loud')
 export class AutoLoudController {
   constructor(private readonly prisma: PrismaService) {}
-  @Cron(CronExpression.EVERY_10_MINUTES, {
+
+  @Cron(CronExpression.EVERY_30_MINUTES, {
     timeZone: 'Asia/Tashkent',
   })
   async avg_Reyting() {
     try {
-      let sum: number = 0;
-      let sch: number = 0;
-      let Avg: number = 0;
-      const SetReyting = new Set<number>();
-      const Reyting = await this.prisma.reyting.findMany();
-      for (let R of Reyting) {
-        SetReyting.add(R.menyu_id);
-      }
-      for (let menyu of SetReyting) {
-        sum = 0;
-        sch = 0;
-        Avg = 0;
-        for (let i of Reyting) {
-          if (menyu == i.menyu_id) {
-            sch += 1;
-            sum += Number(i.ball);
-          }
-        }
-        Avg = parseFloat((sum / sch).toFixed(1));
-        await this.prisma.menyu.update({
-          where: { id: menyu },
-          data: { avg_reytig: Avg },
+      const result = await this.prisma.reyting.groupBy({
+        by: ['menyu_id'],
+        _avg: { ball: true },
+      });
+
+      const updates = result.map((item) => {
+        const avg =
+          item._avg.ball !== null ? parseFloat(item._avg.ball.toFixed(1)) : 0;
+
+        return this.prisma.menyu.update({
+          where: { id: item.menyu_id },
+          data: { avg_reytig: avg },
         });
-      }
+      });
+
+      await this.prisma.$transaction(updates);
     } catch (error) {
-      console.log(error.message);
+      console.error('❌ Xatolik:', error.message);
     }
   }
 
-  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {
+  @Cron('0 9 * * *', {
     timeZone: 'Asia/Tashkent',
   })
   async DeletReyting() {
@@ -49,6 +42,7 @@ export class AutoLoudController {
       console.log(error.message);
     }
   }
+
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {
     timeZone: 'Asia/Tashkent',
   })
